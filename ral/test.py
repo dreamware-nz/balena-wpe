8d2a5435e3382c4f7f136dad7b991fe5b2b28959
John Jansen <john.jansen@me.com>
2019-10-11 10:30:45 +1300

    minimal working system

--- ral/test.py
+++ ral/test.py
@@ -1,12 +1,4 @@
 import time
-import requests
-import logging
 
-logging.info("RAL is alive")
-try:
-    # r = requests.get('http://uix:5000')
-    r = requests.get("https://www.stuff.co.nz")
-    logging.info(r.text)
-except:
-    pass
-time.sleep(30)
+while True:
+    time.sleep(1)
